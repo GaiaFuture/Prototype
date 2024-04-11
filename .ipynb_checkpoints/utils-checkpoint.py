2c46d999a025a2aabf6b42655d033c6bf2050f7c
saingersoll <sofia.ingersoll@Outlook.com>
2024-04-11 10:53:31 -0600

    GPR plotting function is plotting as expected! Updated the x_values to be included in plotting .predict() function

--- .ipynb_checkpoints/utils-checkpoint.py
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -27,6 +27,11 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import r2_score
 from scipy.stats import norm
+from scipy.fft import fft
+
+import panel as pn
+import param
+pn.extension()
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ----  server request to aid processing  ----
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -207,12 +212,11 @@ def wrangle_var_cluster(da):
     return var
 
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# ----  User Selected ML Plotting Funct   ----
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# function to plot a cluster for to build on for ml
+# Create an array that sets the value of all 32 parameters to 0.5
+# this will be used when plotting emulation
+X_values = np.full((10, 32), 0.5)  # Fill array with 0.5
+
 def train_emulator(param, var):
-    '''Train the emulator based on the selected parameter and variable'''
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # ----      Split Data 90/10        ----
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -220,57 +224,215 @@ def train_emulator(param, var):
     X_train, X_test, y_train, y_test = train_test_split(param,
                                                         var,
                                                         test_size=0.2,
-                                                       # setting a seed
+                                                        # setting a seed
                                                         random_state=0)
 
-    gpr_model = GaussianProcessRegressor(normalize_y=True)
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # ----    Kernel Specs No Tuning    ----
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # initiate the model without tuning
+    kernel = ConstantKernel(constant_value = 3,
+                            constant_value_bounds=(1e-2, 1e4)) \
+                  * RBF(length_scale=1, 
+                        length_scale_bounds=(1e-4, 1e8))
 
-    gpr_model.fit(X_train, y_train)
+    gpr_model = GaussianProcessRegressor(kernel=kernel,
+                                        n_restarts_optimizer=20,
+                                        random_state=42,
+                                        normalize_y=True)
 
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # ----         Fit the Model        ----
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Fit the model to the training data
+    gpr_model = gpr_model.fit(X_train, y_train)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # ----         Get Predictions      ----
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Make predictions
     y_pred, y_std = gpr_model.predict(X_test, return_std=True)
+
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # ----         Collect Metrics      ----
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Verify training score
+    train_score = gpr_model.score(X_train, y_train)
+
+    # Accuracy Score
+    #accuracy = accuracy_score(y_test, y_pred)
+
     # Calculate Mean Absolute Error
     mae = mean_absolute_error(y_test, y_pred)
-
+    
     # Calculate R^2
     r2_train = r2_score(y_test, y_pred)
+    
     # Calculate RMSE
     rmse_train = np.sqrt(mean_squared_error(y_test, y_pred))
 
     # Create a DataFrame to store the results for plotting
     results_df = pd.DataFrame({
-         'y_pred': y_pred,
+        'y_pred': y_pred,
         'y_std': y_std,
         'y_test': y_test,
-         'X_test': [x.tolist() for x in X_test],  # Convert array to list for DataFrame
+        'X_test': [x.tolist() for x in X_test],  # Convert array to list for DataFrame
     })
 
     # Add metrics to the DataFrame
     results_df['R^2'] = r2_train
     results_df['RMSE'] = rmse_train
+    #results_df['Accuracy Score'] = accuracy
     results_df['Mean Absolute Error'] = mae
+    
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # ----      Pickle Emulation     ----
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+
+
+
+    
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # ----        Print Metrics         ----
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Print Training Metrics
+  #  print("Accuracy Score:", accuracy)
     print("Training R^2:", r2_train)
     print("Training RMSE:", rmse_train)
     print("Mean Absolute Error:", mae)
+    print("Training Score:", train_score)
 
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# ---- User Selected Panel Plotting Funct ---- # adding this because working to implement into ml wkflw 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# function to plot a cluster for the dashboard
-def cluster_panel_plot(param_avg, var_avg):
-    '''Basic plotting to aid with dashboard set up 
-    building on currently.'''
-    data = pd.DataFrame({'x': param_avg, 'y': var_avg})
     
-    # Create scatter plot using hvplot
-    hvplot.scatter(
-        x='x', y='y', color='#62c900ff', alpha=0.8,
-        xlabel=param, ylabel=var, title='2005-2010 Global Average'
-    )
-    return pn.panel(scatter_plot)
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # ----      Visualize Emulation     ----
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Calculate the z-score for the 99.7% confidence interval
+    # 99.7th percentile (three standard deviations)
+    z_score = norm.ppf(0.99865)  
+    
+    #For the parameter of interest, replace the 0.5 with a range of values between 0 and 1
+    X_values[:, 15] = np.linspace(0, 1, 10)  # Set the 15th column values to evenly spaced values from 0 to 1
+    coef_deter = r2_score(y_test,y_pred)
+
+    
+    # Plot the results
+    plt.figure(figsize=(10, 6))
+    
+    plt.plot(X_values[:, 15],
+             y_pred[:10,],
+             color='#134611',
+             label='GPR Prediction')
+
+    plt.text(0,np.max(y_test),
+             'R2_score = '+str(np.round(coef_deter,2)),
+             fontsize=10)
+    
+    # applying z-score for 99.7% CI
+    plt.fill_between(X_values[:, 15],
+                     y_pred[:10] - z_score * y_std[:10], y_pred[:10] + z_score * y_std[:10],
+                     alpha=0.5, 
+                     color='#9d6b53',
+                     label = '3 St.Dev., Confidence Interval')
+
+   
+    
+    plt.xlabel('Perturbed Parameter: ')
+    plt.ylabel('Variable: ')
+    plt.title('Parameter Perturbation Uncertainty Estimation')
+    
+    plt.legend()
+    plt.show()
+
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # ----      Visualize Accuracy      ----
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
+    plt.errorbar(y_test,
+                 y_pred,
+                 yerr=3*y_std,
+                 fmt="o",
+                 color='#134611')
+    
+    plt.text(-0.3,np.max(y_test),
+             'R2_score = '+str(np.round(coef_deter,2)),
+             fontsize=10)
+    
+    plt.plot([0,np.max(y_test)],
+             [0,np.max(y_pred)],
+             linestyle='--',
+             c='k')
+    
+    plt.xlim([np.min(y_test)-1,np.max(y_test)+1])
+    plt.ylim([np.min(y_pred)-1,np.max(y_pred)+1])
+
+    plt.xlabel('Variable Test')
+    plt.ylabel('Emulated Variable')
+    plt.title('Emulator Validation')
+    
+    plt.tight_layout()
+
+    return plt.show()
+
+
+
+# Define a custom function to generate the Gaussian regression line for each parameter
+def gaussian_regression_lines(model, X):
+    fourier_amplitudes = []  # List to store Fourier amplitudes for each parameter
+    
+    for param_index in range(32):
+        # Generate x_values with 32 dimensions
+        x_values = np.full((10, 32), 0.5)  # Fill array with 0.5
+        x_values[:, param_index] = np.linspace(0, 1, 10)  # Set the current parameter values to evenly spaced values from 0 to 1
+
+        # Predict mean and standard deviation of the Gaussian process at each point in x_values
+        y_mean, _ = model.predict(X_values, return_std=True)
+
+        # Compute Fourier transform of the model output
+        y_fft = fft(y_mean)
+
+        # Compute amplitude of each frequency component
+        amplitude = np.abs(y_fft)
+
+        # Store the amplitude corresponding to the first non-zero frequency (excluding DC component)
+        fourier_amplitudes.append(amplitude[1])
+
+    return fourier_amplitudes
+
+def dashboard_wrangling(param, var):
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #----            Parameter Data.          ----
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # NEED TO ADD NAME ATTRIBUTE IN WRANGLING PORTION
+    params = param_wrangling()
+    
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #----        If-else Load Data       ----
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    filepath = os.path.join("saves", f"{var}.nc")
+    if os.path.exists(filepath):
+         #read in the file as a dataset
+        ds=xr.open_dataset('saves/'+var+'.nc')
+    
+        #then convert back to data array
+        var_avg = ds[var]
+    else:
+        print(f"Reading and wrangling your data, this may take a few minutes")
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # ----    Subset User Selection Funct     ----
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        var_da = subset_var_cluster(var)
+
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # ----      Subset Var Wrangle Funct      ----
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # NEED TO ADD NAME ATTRIBUTE IN WRANGLING PORTION
+        var_avg = wrangle_var_cluster(var_da)
+
+        #you ought to convert the data array to dataset before writing to file
+        ds = var_avg.to_dataset(name = var)
+        ds.to_netcdf('saves/'+var+'.nc') # note that this will throw error if you try to overwrite existing files
+
+    return train_emulator(params, var_avg)
